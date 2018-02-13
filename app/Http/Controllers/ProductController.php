9d170745e06e9e1278ae51f88ff771f41692ee15
Nixon Kosgei <nkosgey6@gmail.com>
2018-02-14 02:44:27 +0300

    now fetching products from the database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('products.index');
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
     }
 
     /**
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,21 +12,20 @@
         
 </div>
 <div class="row">
-    @for ($i = 0; $i < 10; $i++)
-    <a href="{{ route('products.show', ['id' => $i]) }}" class="col">
+    @foreach ($products as $product)
+    <a href="{{ route('products.show', ['id' => $product->id ]) }}" class="col">
         <div class="card">
-            <div class="card-image" style=" background-image: url(https://unsplash.it/1200/600?image=82);">
+            <div class="card-image" style=" background-image: url({{asset('uploads')}}/{{ $product->image }});">
             </div>
             <div class="card-content">
                 <button class="card-fab fab"><i class="mdi mdi-cart-plus"></i></button>
-                <div class="card-title">Product Name</div>
-                <?php $string = 'There are 3 main button types described in material design. The raised button is a standard button that signify actions and seek to give depth to a mostly flat page. The floating circular action button is meant for very important functions' ?>
-                <p class="card-text">{{ str_limit($string, 100, $end = '...') }}</p>
-                <p><b>Ksh</b> 5,900</p>
+                <div class="card-title">{{ $product->name }}</div>
+                <p class="card-text">{{ str_limit($product->description, 100, $end = '...') }}</p>
+                <p><b>Ksh</b> {{ $product->price }}</p>
             </div>
         </div>
     </a>
-    @endfor
+    @endforeach
     
 </div>
 
