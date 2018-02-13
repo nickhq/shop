a864a8d967bf129caf2d8fa6f5fbd9a91eb0318f
Nixon Kosgei <nkosgey6@gmail.com>
2018-02-13 16:15:23 +0300

    itterated over a single item in index view, added product price

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,19 +3,29 @@
 @section('title', 'All Products')
 
 @section('content')
-<h1>Our Products</h1>
+
+<div class="clearfix">
+        <h1>Our Products
+                <a class="to-right btn" href="{{ route('products.create') }}">Add Product</a>
+            </h1> 
+        
+</div>
 <div class="cards">
+    @for ($i = 0; $i < 10; $i++)
     <a href="#" class="cards-item">
         <div class="card">
-        <div class="card-image" style=" background-image: url(https://unsplash.it/800/600?image=82);">
-        </div>
-        <div class="card-content">
-            <button class="card-action fab"><i class="mdi mdi-cart-plus"></i></button>
-            <div class="card-title">Flex</div>
-            <p class="card-text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
-        </div>
+            <div class="card-image" style=" background-image: url(https://unsplash.it/800/600?image=82);">
+            </div>
+            <div class="card-content">
+                <button class="card-fab fab"><i class="mdi mdi-cart-plus"></i></button>
+                <div class="card-title">Product Name</div>
+                <?php $string = 'There are 3 main button types described in material design. The raised button is a standard button that signify actions and seek to give depth to a mostly flat page. The floating circular action button is meant for very important functions' ?>
+                <p class="card-text">{{ str_limit($string, 100, $end = '...') }}</p>
+                <p><b>Ksh</b> 5,900</p>
+            </div>
         </div>
     </a>
+    @endfor
     
 </div>
 
