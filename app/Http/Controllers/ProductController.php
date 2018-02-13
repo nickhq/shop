6746720e520fda6207b2a54995b47f106cce3e5d
Nixon Kosgei <nkosgey6@gmail.com>
2018-02-13 23:50:49 +0300

    backend validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,6 +35,22 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'product_name' => 'required',
+            'product_image' => 'required|image64:jpeg,jpg,png,svg,bmp,gif',
+            'product_description' => 'required|min:30',
+            'product_price' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors'=>$validator->errors()]);
+        } else {
+            $imageData = $request->get('product_image');
+            $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.' . explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
+            Image::make($request->get('image'))->save(public_path('uploads/').$fileName);
+            return response()->json(['error'=>false]);
+        }
+
         return $request->all();
     }
 
